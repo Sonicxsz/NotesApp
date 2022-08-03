9e0f89877bbd38c2f6401724e469f87b9d2747f8
Arbi <arbih@mail.ru>
2022-08-03 03:16:25 +0300

    update single page added settings panel

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import NoteItemList from './components/noteItem/NoteItemList';
-import Menu from './components/menu/menu';
-import { useAppDispatch, useAppSelector } from './hooks';
-import Modal from './components/modal/modal';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
-import { fetchNotes } from './store/slice/notesSlice';
-import './App.css';
-
-
-
-
+import React, { useEffect, useState } from "react";
+import NoteItemList from "./components/noteItem/NoteItemList";
+import Menu from "./components/menu/menu";
+import { useAppDispatch, useAppSelector } from "./hooks";
+import Modal from "./components/modal/modal";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { fetchNotes } from "./store/slice/notesSlice";
+import "./App.css";
+import SingleNote from "./components/pages/singleNote";
 
 function App() {
-  
-  const [isNoteOpen, setNoteOpen] = useState<boolean>(false)
-  const dispatch = useAppDispatch()
-  const loading = useAppSelector(state => state.notesSlice.loading)
-  useEffect(() =>{
-    dispatch(fetchNotes())
-  },[])
+  const [isNoteOpen, setNoteOpen] = useState<boolean>(false);
+  const dispatch = useAppDispatch();
+  const loading = useAppSelector((state) => state.notesSlice.loading);
+  useEffect(() => {
+    dispatch(fetchNotes());
+  }, []);
 
-  useEffect(() =>{
-    dispatch(fetchNotes())
-  },[loading])
-
+  useEffect(() => {
+    dispatch(fetchNotes());
+  }, [loading]);
 
   return (
     <div className="App">
-      <Menu openNote={setNoteOpen}/>
-      {isNoteOpen ? <Modal closeNote={setNoteOpen}/> : null}
+      <Menu openNote={setNoteOpen} />
+      {isNoteOpen ? <Modal closeNote={setNoteOpen} /> : null}
       <BrowserRouter>
-      <Routes >
-    
-      <Route path='/' element={<NoteItemList />}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<NoteItemList />} />
+          <Route path="/noteid:id" element={<SingleNote />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
--- src/components/modal/modalNote.tsx
+++ src/components/modal/modalNote.tsx
@@ -1,105 +1,102 @@
-import { Box, Button, TextField, Typography } from '@mui/material'
-import { nanoid } from 'nanoid'
-import { title } from 'process'
-import style from './modal.module.scss'
-import { useState } from 'react'
-import { useAppDispatch } from '../../hooks'
-import { addNotes } from '../../store/slice/notesSlice'
-import { timeCreater } from '../../utils/time'
-import { Istate } from '../noteItem/NoteItemList'
-import ModalBtns from './modalBtns'
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { nanoid } from "nanoid";
+import { title } from "process";
+import style from "./modal.module.scss";
+import { useState } from "react";
+import { useAppDispatch } from "../../hooks";
+import { addNotes } from "../../store/slice/notesSlice";
+import { timeCreater } from "../../utils/time";
+import { Istate } from "../noteItem/NoteItemList";
+import ModalBtns from "./modalBtns";
 
 interface Imodal {
-    closeNote: (value:boolean) => void
-
+  closeNote: (value: boolean) => void;
 }
 
-function ModalNote(props:Imodal) {
-    const [name, setName] = useState<string>('')
-    const [title, setTitle] = useState<string>('')
-    const [colorBtns, setColorBtns] = useState([
-        {id: 0, name: "#2E958C"},
-        {id: 1, name: "#4ca65f"},
-        {id: 2, name: "#c35fcc"},
-        {id: 3, name: "#e57a2d"},
-    ])
-    const [color, setColor] = useState<string>("#2E958C")
+function ModalNote(props: Imodal) {
+  const [name, setName] = useState<string>("");
+  const [title, setTitle] = useState<string>("");
+  const [colorBtns, setColorBtns] = useState([
+    { id: 0, name: "#2E958C" },
+    { id: 1, name: "#4ca65f" },
+    { id: 2, name: "#c35fcc" },
+    { id: 3, name: "#e57a2d" },
+  ]);
+  const [color, setColor] = useState<string>("#2E958C");
 
-    const dispatch = useAppDispatch()
-    function addNewNote (){
-            let id = nanoid()
-            let date = timeCreater()
-            let newNote:Istate = {
-                important: false,
-                name: name,
-                title,
-                id,
-                time: date,
-                color,
-
-            }
-            const json = JSON.stringify(newNote)
-            dispatch(addNotes(json))
-            props.closeNote(false)
-            
-        }
-    
+  const dispatch = useAppDispatch();
+  function addNewNote() {
+    let id = nanoid();
+    let date = timeCreater();
+    let newNote: Istate = {
+      important: false,
+      name: name,
+      title,
+      id,
+      time: date,
+      color,
+    };
+    const json = JSON.stringify(newNote);
+    dispatch(addNotes(json));
+    props.closeNote(false);
+  }
 
   return (
     <>
-     <Box>
-            <div className={style.flex}>
-            
-            <TextField 
-            value={name}
-            onChange={(e) =>{
-                setName(e.target.value)
-            }}
-            className={style.nameInp} 
-            id="standard-basic" 
-            label="Введите название заметки" 
-            variant="standard" />
-            
-        </div>
-       
-      
+      <Box>
         <div className={style.flex}>
-        <TextField
+          <TextField
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            className={style.nameInp}
+            id="standard-basic"
+            label="Введите название заметки"
+            variant="standard"
+          />
+        </div>
+
+        <div className={style.flex}>
+          <TextField
             minRows={2}
             maxRows={6}
             value={title}
-            onChange={(e) =>{
-                setTitle(e.target.value)
+            onChange={(e) => {
+              setTitle(e.target.value);
             }}
-          className={style.textInp}
-          id="standard-textarea"
-          label="Пишите...."
-          placeholder="Творите"
-          multiline
-          variant="standard"
-        />
-      
-      
+            className={style.textInp}
+            id="standard-textarea"
+            label="Пишите...."
+            placeholder="Творите"
+            multiline
+            variant="standard"
+          />
         </div>
-            </Box>
-            <div className={style.colors}>
-                <Typography mr={1} variant='body1'>
-                    Выберите цвет:
-                </Typography>
-                {colorBtns.map(i =>{
-                    let clazz = i.name === color ? `${style.colorPick}` : `${style.color}`
-                    return <div
-                    onClick={() =>{
-                        setColor(i.name)
-                    }}
-                    key={i.id} className={clazz} style={{backgroundColor: i.name}} ></div>
-                })}
-            </div>
-            
-            <ModalBtns addNewNote={addNewNote} closeNote={props.closeNote}/>
-           
+      </Box>
+      <div className={style.colors}>
+        <Typography mr={1} variant="body1">
+          Выберите цвет:
+        </Typography>
+        {colorBtns.map((i) => {
+          let clazz =
+            i.name === color ? `${style.colorPick}` : `${style.color}`;
+          return (
+            <div
+              onClick={() => {
+                setColor(i.name);
+              }}
+              key={i.id}
+              className={clazz}
+              style={{ backgroundColor: i.name }}
+            ></div>
+          );
+        })}
+      </div>
+
+      <ModalBtns addNewNote={addNewNote} closeNote={props.closeNote} />
     </>
-  )
+  );
 }
 
-export default ModalNote
+export default ModalNote;
